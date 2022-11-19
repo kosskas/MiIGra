787f92cc1292717f5016c0156dbf3308200bd835
kosskas <matipolska637@gmail.com>
2022-11-19 20:13:50 +0100

    Stan na 19.11.22

---
+++ Assets/Scripts/FoxController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0.1f;
+    public float jumpForce = 6.0f;
+    private Rigidbody2D rigidBody;
+    public LayerMask groundLayer;
+    const float rayLength = 0.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.Translate(moveSpeed, 0.0f, 0.0f, Space.World);
+
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+       // Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1,false);
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
+    }
+
+    void Jump()
+    {
+        if(isGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("jumping");
+        }
+    }
+}
