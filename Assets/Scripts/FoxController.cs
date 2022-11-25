a4987e2af93e0c29cd469bebbcba3f065b410271
kosskas <93159483+kosskas@users.noreply.github.com>
2022-11-25 18:27:12 +0100

    Fox Animaton
    Podstawowa animacja lisa

--- Assets/Scripts/FoxController.cs
+++ Assets/Scripts/FoxController.cs
@@ -9,51 +9,78 @@ public class FoxController : MonoBehaviour
     private Rigidbody2D rigidBody;
     public LayerMask groundLayer;
     const float rayLength = 0.9f;
+    private Animator animator;
+    private bool isWalking = false;
+    private bool isFacingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    //test1
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       // transform.Translate(moveSpeed, 0.0f, 0.0f, Space.World);
+        // transform.Translate(moveSpeed, 0.0f, 0.0f, Space.World);
+        HandleMovement();
+        animator.SetBool("IsGrounded", isGrounded());
+        animator.SetBool("IsWalking", isWalking);
 
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        // Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1,false);
+    }
+
+
+
+    private void HandleMovement()
+    {
+        //Prawo
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (!isFacingRight)
+                Flip();
         }
-
+        //Lewo
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (isFacingRight)
+                Flip();
         }
-
-        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        //Góra
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-
-       // Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1,false);
     }
 
-    bool isGrounded()
-    {
-        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
-    }
-
-    void Jump()
+    private void Jump()
     {
         if(isGrounded())
         {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             Debug.Log("jumping");
         }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+    private bool isGrounded()
+    {
+        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
     }
 }
